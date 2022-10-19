cb9bddec721300ef5e0230cf181e16ca4e3e3910
thiagokenzo2002 <RM87150@fiap.com.br>
2022-10-19 12:24:19 -0300

    first commit

--- login.js
+++ login.js
@@ -40,7 +40,7 @@ function entrar() {
 
     
     if(usuario.value == userValid.user && senha.value == userValid.senha){
-        window.location.href = 'https://cadastro-login.vercel.app/inicio.html'
+        window.location.href = 'http://127.0.0.1:5500/inicio.html'
 
        let token = Math.random().toString(16)
        localStorage.setItem('token', token)
